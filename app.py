223054110d7d4d391d23bc043d879e70cc3e4b11
ghosang <ghosang@asb9812u-a01.csil.sfu.ca>
2018-05-14 17:42:30 -0700

    Added template and layout html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/'):
-	def index():
-		return 'INDEX'
+@app.route('/')
+def index():
+	return render_template('home.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
