9281bf2ba6421d23ce49596d3d9741e26f7d961a
Giovanni HoSang <giovanni.hosang@gmail.com>
2018-05-15 22:02:06 -0700

    BugFix: Changed error in article route

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def articles():
 	return render_template('articles.html', articles = Articles)
 
 @app.route('/article/<string:id>/')
-def articles(id):
+def article(id):
 	return render_template('article.html', id=id)
 
 if __name__ == '__main__':
