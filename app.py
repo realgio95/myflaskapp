6f4d38526e8a249c04794e59fb27afdc71e1ce1f
Giovanni HoSang <giovanni.hosang@gmail.com>
2018-05-21 21:47:16 -0700

    Fixed All routing issues

--- app.py
+++ app.py
@@ -5,21 +5,25 @@ app = Flask(__name__)
 
 Articles = Articles()
 
+
 @app.route('/')
 def index():
-	return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/articles')
 def articles():
-	return render_template('articles.html', articles = Articles)
+    return render_template('articles.html', articles=Articles)
+
 
 @app.route('/article/<string:id>/')
 def article(id):
-	return render_template('article.html', id=id)
+    return render_template('article.html', id=id)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
--- data.py
+++ data.py
@@ -1,29 +1,29 @@
 def Articles():
-	articles = [
-		{
-			'id': 1,
-			'name': 'Article One',
-			'body': 'Lorem ipsum dolor sit amet, qui no mutat utamur assueverit, magna delectus molestiae nam at. Ut affert iudico voluptatibus ius, no vide postea impetus eum. No pri feugait instructior. Error impedit sed ea. Eum ne discere recteque convenire, te melius iuvaret abhorreant est. Sed stet errem id. Meliore iracundia nam ei, qui iudico forensibus cu.',
-			'author': 'Brad Traversy',
-			'create_date': '04-25-2017'
-		}, {
-			'id': 2,
-			'name': 'Article Two',
-			'body': 'Qui no mutat utamur assueverit, magna delectus molestiae nam at. Ut affert iudico voluptatibus ius, no vide postea impetus eum. No pri feugait instructior. Error impedit sed ea. Eum ne discere recteque convenire, te melius iuvaret abhorreant est. Sed stet errem id. Meliore iracundia nam ei, qui iudico forensibus cu.',
-			'author': 'John Traversy',
-			'create_date': '04-26-2017'
-		}, {
-			'id': 3,
-			'name': 'Article Three',
-			'body': 'Magna delectus molestiae nam at. Ut affert iudico voluptatibus ius, no vide postea impetus eum. No pri feugait instructior. Error impedit sed ea. Eum ne discere recteque convenire, te melius iuvaret abhorreant est. Sed stet errem id. Meliore iracundia nam ei, qui iudico forensibus cu.',
-			'author': 'Brandy Macmillan',
-			'create_date': '04-27-2017'
-		}, {
-			'id': 4,
-			'name': 'Article Four',
-			'body': 'Utamur assueverit, magna delectus molestiae nam at. Ut affert iudico voluptatibus ius, no vide postea impetus eum. No pri feugait instructior. Error impedit sed ea. Eum ne discere recteque convenire, te melius iuvaret abhorreant est. Sed stet errem id. Meliore iracundia nam ei, qui iudico forensibus cu.',
-			'author': 'Johnny Diapinsnagen',
-			'create_date': '04-29-2017'
-		}
-	]
-	return articles;
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, qui no mutat utamur assuever',
+            'author': 'Brad Traversy',
+            'create_date': '04-25-2017'
+        }, {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Qui no mutat utamur assueverit, molestiae',
+            'author': 'John Traversy',
+            'create_date': '04-26-2017'
+        }, {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Magna delectus molestiae nam at. Ut affert iudi.',
+            'author': 'Brandy Macmillan',
+            'create_date': '04-27-2017'
+        }, {
+            'id': 4,
+            'title': 'Article Four',
+            'body': 'Utamur assueverit, magna delectus at.',
+            'author': 'Johnny Diapinsnagen',
+            'create_date': '04-29-2017'
+        }
+    ]
+    return articles
