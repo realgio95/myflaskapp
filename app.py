28238b8cdb8d1f97898edaef4e427bfc13903d2d
Giovanni HoSang <giovanni.hosang@gmail.com>
2018-05-23 01:15:54 -0700

    Added modules adn imported them for future use

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
