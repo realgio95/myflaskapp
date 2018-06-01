5771c3a79e026a2b75208498cc0569aab2e30466
Giovanni HoSang <giovanni.hosang@gmail.com>
2018-05-31 17:26:10 -0700

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-def DBconfig():
-    dbconfig = {
-        "host": 'localhost',
-        "user": 'root',
-        "password": 'Dappa95!',
-        "DBName": 'myflaskapp',
-        "dictDB": 'DictCursor',
-    }
-    return dbconfig
